f204bfad5f2ecdc6bb8a596e9f6930862378f722
vsotelo <vsotelo@linkfast.io>
2026-05-20 10:49:01 -0500

    Configure as static site for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://qhapaq.com',
+  output: 'static',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
